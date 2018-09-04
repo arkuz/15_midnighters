1c4e91e618c2b18e2db54fc8a3f3101ad9d90d3d
arkuz <arkuzka@mail.ru>
2018-09-04 21:56:21 +0300

    tmp commit, changed generator func

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,39 @@
+import requests
+import pytz
+
+def get_solution_attempts():
+    endpoint = 'https://devman.org/api/challenges/solution_attempts/'
+    response = requests.get(endpoint)
+    if response == 400:
+        return None
+    else:
+        return response.json()
+
+
+
 def load_attempts():
     pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+    while True:
+        url = 'http://devman.org/api/challenges/solution_attempts/?page='\
+              + str(pages)
+        response = requests.get(url)
+        resp = response.json()
+        pages_limit = resp['number_of_pages']
+        yield resp
+        if pages == pages_limit:
+            break
+        pages += 1
+
 
 def get_midnighters():
+
     pass
 
 if __name__ == '__main__':
-  pass
+
+
+
+    for attempt in load_attempts():
+        print(attempt)
+
+    #print(get_solution_attempts())
