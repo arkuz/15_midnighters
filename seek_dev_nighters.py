4c949e2df25e4952ccb12670d3c5a869dea51c7c
arkuz <arkuzka@mail.ru>
2018-09-10 20:36:45 +0300

    fixed comments

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -8,35 +8,26 @@ def load_attempts():
     while True:
         url = 'http://devman.org/api/challenges/solution_attempts/'
         params = {'page': pages}
-        response_json = requests.get(url, params).json()
-        yield response_json
-        if pages == response_json['number_of_pages']:
+        response = requests.get(url, params).json()
+        yield response
+        if pages == response['number_of_pages']:
             break
         pages += 1
 
 
 def get_midnighters(pages):
-    user_list = []
+    user_list = set()
+    start_hour = 0
+    end_hour = 5
     for page in pages:
         for record in page['records']:
-            current_timezone = pytz.timezone(
-                record['timezone']
-            )
             current_datetime = datetime.datetime.fromtimestamp(
-                record['timestamp']
+                record['timestamp'],
+                tz=pytz.timezone(record['timezone'])
             )
-            current_datetime_in_tz = current_datetime.astimezone(
-                current_timezone
-            )
-            user_info = {
-                'user': record['username'],
-                'time': current_datetime_in_tz.strftime('%H:%M:%S')
-            }
-            hour = current_datetime_in_tz.hour
-            start_hour = 0
-            end_hour = 5
-            if int(hour) >= start_hour and end_hour >= int(hour):
-                user_list.append(user_info)
+            hour = current_datetime.hour
+            if hour >= start_hour and end_hour >= hour:
+                user_list.add(record['username'])
     return user_list
 
 
@@ -45,7 +36,7 @@ def print_user_list(user_list):
         print('User list is empty.')
     else:
         for user in user_list:
-            print('  {0} - {1}'.format(user['user'], user['time']))
+            print('  {0}'.format(user))
 
 
 if __name__ == '__main__':
